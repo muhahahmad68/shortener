714a5a0f7b5bd3f17364c1f9c639ea999f42e6a4
Muhammad Ademola <75504094+muhahahmad68@users.noreply.github.com>
2023-10-18 14:10:31 +0000

    Fisrt commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// load all url from redirect.yml
+const YAML = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirect.yml'), 'utf-8')
+const redirect = YAML.parse(redirectsFile)
+
+console.log(redirect)
+
+// Generate html page for each redirect url from template.html
+
+const templateHTML = fs.readFileSync(path.join(__dirname, 'templates.html'), 'utf-8')
+
+// Loop through all redirect and generate page
+
+for(let[slug, url] of Object.entries(redirect)) {
+    console.log("Generating HTML page for ", slug)
+}
